6f6d0cb826fa3b3a34ef8e8774760161b7f6541a
codelala <93863020+Axyszce@users.noreply.github.com>
2023-02-12 21:30:13 +0800

    wait
    dfdfdfg
    
    Co-Authored-By: AJbrinas <66823178+AJBrinas@users.noreply.github.com>

---
+++ index.php
@@ -0,0 +1,99 @@
+<html>
+<head>
+    <title>PUPQC MERCH</title>
+    </head>
+    <link rel="stylesheet" href="css/home.css">
+    <body>
+        <nav class="navbar">
+            <div class="nav">
+                <img src="img/logo.png" class="logo" alt="">
+                <div class="nav-items">
+                    <div class="search">
+                        <input type="text" class="search-box" placeholder="search brand, product">
+                        <button class="search-btn">search</button>
+                    </div>
+                    <a href="#"><img src="img/user.png" alt=""></a>
+                    <a href="#"><img src="img/cart.png" alt=""></a>
+                </div>
+            </div>
+            <ul class="links-container">
+                <li class="link-item"><a href="#" class="link">Home</a></li>
+                <li class="link-item"><a href="#" class="link">Products</a></li>
+                <li class="link-item"><a href="#" class="link">About Us</a></li>
+                <li class="link-item"><a href="#" class="link">Size Chart</a></li>
+                <li class="link-item"><a href="#" class="link">FAQs</a></li>
+            </ul>
+            </nav>
+            <!---Slideshow-->
+        <link rel="stylesheet" href="css/style.css">
+        <script src="js/slideshow.js" defer></script>
+            <header class="intro">
+                <div class="intro-slideshow">
+            <img src="1.jpg" class="slideshow-img">
+            <img src="2.jpg" class="slideshow-img">
+            <img src="banner-img3.jpg" class="slideshow-img">
+            <img src="banner-img4.jpg" class="slideshow-img">
+            </div>
+            <div class="intro-header">
+                <h1>PUP Merch</h1>
+                <p>SHOP Now</p>
+            </div>
+            </header>
+            <!--product-->
+            <section class="product">
+                <h2 class="product-category">best selling</h2>
+                <button class="pre-btn"><img src="img/arrow.png" alt=""></button>
+                <button class="nxt-btn"><img src="img/arrow.png" alt=""></button>
+                <div class="product-container">
+                    <div class="product-card">
+                        <div class="product-image">
+                            <span class="discount-tag">50% off</span>
+                            <img src="img/card1.png" class="product-thumb" alt="">
+                            <button class="card-btn">add to whislist</button>
+                        </div>
+                        <div class="product-info">
+                            <h2 class="product-brand">brand</h2>
+                            <p class="product-short-des">a short line about the cloth..</p>
+                            <span class="price">$20</span><span class="actual-price">$40</span>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!--product-->
+            <section class="product">
+                <div class="product-container">
+                    <div class="product-card">
+                        <div class="product-image">
+                            <span class="discount-tag">50% off</span>
+                            <img src="img/card1.png" class="product-thumb" alt="">
+                            <button class="card-btn">add to whislist</button>
+                        </div>
+                        <div class="product-info">
+                            <h2 class="product-brand">brand</h2>
+                            <p class="product-short-des">a short line about the cloth..</p>
+                            <span class="price">$20</span><span class="actual-price">$40</span>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!--collections-->
+            <section class="collection-container">
+                <a href="#" class="collection">
+                    <img src="img/women-collection.png" alt="">
+                    <p class="collection-title">women <br> apparels</p>
+                </a>
+                <a href="#" class="collection">
+                    <img src="img/men-collection.png" alt="">
+                    <p class="collection-title">men <br> apparels</p>
+                </a>
+                <a href="#" class="collection">
+                    <img src="img/accessories-collection.png" alt="">
+                    <p class="collection-title">accessories</p>
+                </a>
+            </section>
+
+            <script src="js/nav.js"></script>
+            <script src="js/home.js"></script>
+
+</body>
+</html>
